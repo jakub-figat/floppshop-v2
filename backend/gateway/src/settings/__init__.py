8cf0b67062237ca101ded36517180fd3de635bd1
jakub-figat <jakub.figat@skygate.io>
2021-11-24 10:53:22 +0100

    Fix test API paths

--- backend/gateway/src/settings/__init__.py
+++ backend/gateway/src/settings/__init__.py
@@ -1,5 +1,6 @@
 from src.settings.database import DatabaseSettings
 from src.settings.general import GeneralSettings
+from src.settings.api_urls import APIUrls
 
 
 class Settings(GeneralSettings, DatabaseSettings):
