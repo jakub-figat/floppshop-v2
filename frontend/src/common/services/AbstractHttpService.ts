8d00b6e6d3d794c5a2cee54181eda623ea8fa01a
Patryk Harasiuk <patryk.harasiuk.skygate@gmail.com>
2021-12-04 14:20:29 +0100

    [fs-5] add abstract http service

---
+++ frontend/src/common/services/AbstractHttpService.ts
@@ -0,0 +1,66 @@
+const enum HttpRequestMethod {
+    GET = 'GET',
+    POST = 'POST',
+    PUT = 'PUT',
+    DELETE = 'DELETE',
+    PATCH = 'PATCH',
+}
+
+type GenericHttpRequestParams = {
+    url: string,
+    responseType?: number,
+    withCredentials?: boolean,
+}
+
+type MakeRequestParams = {
+    url: string,
+    method: HttpRequestMethod,
+    body?: unknown;
+    responseType?: unknown
+    withCredentials?: boolean;
+}
+
+type HttpGetRequest = GenericHttpRequestParams;
+type HttpPostRequest = GenericHttpRequestParams & { body?: unknown };
+type HttpPutRequest = GenericHttpRequestParams & { body?: unknown };
+type HttpDeleteRequest = GenericHttpRequestParams;
+
+export abstract class AbstractHttpService {
+
+    public get({url} : HttpGetRequest) : Promise<any> {
+        return this.makeRequest({
+            url,
+            method: HttpRequestMethod.GET
+        })
+    }
+
+    public post({url}: HttpPostRequest) : Promise<any> {
+        return this.makeRequest({
+            url,
+            method: HttpRequestMethod.POST
+        })
+    }
+
+    public put({url}: HttpPutRequest) : Promise<any> {
+        return this.makeRequest({
+            url,
+            method: HttpRequestMethod.PUT
+        })
+    }
+
+    public delete({url}: HttpDeleteRequest) : Promise<any> {
+        return this.makeRequest({
+            url,
+            method: HttpRequestMethod.DELETE
+        })
+    }
+
+    protected abstract makeRequest({
+        url,
+        method,
+        responseType,
+        withCredentials
+}: MakeRequestParams) : Promise<any>
+
+
+} 
