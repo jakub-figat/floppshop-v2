757b894c3bbc723f11d9d2a4671b4a8e558efbd2
jakub-figat <jakub.figat@skygate.io>
2021-12-04 09:57:32 +0100

    Add camelCase renderer/parser

--- backend/gateway/src/apps/product/schemas.py
+++ backend/gateway/src/apps/product/schemas.py
@@ -1,12 +1,17 @@
 from decimal import Decimal
 from uuid import UUID
 
+from humps import camelize
 from pydantic import BaseModel
 
 
 class CategoryOutputSchema(BaseModel):
     id: UUID
     name: str
+
+    class Config:
+        alias_generator = camelize
+        allow_population_by_field_name = True
 
 
 class ProductOutputSchema(BaseModel):
@@ -15,6 +20,14 @@ class ProductOutputSchema(BaseModel):
     price: Decimal
     categories: list[CategoryOutputSchema]
 
+    class Config:
+        alias_generator = camelize
+        allow_population_by_field_name = True
+
 
 class ProductAddInputSchema(BaseModel):
     count: int
+
+    class Config:
+        alias_generator = camelize
+        allow_population_by_field_name = True
